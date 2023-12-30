19e29ee4edf61e68b2047ad7a100ca0db31b71d6
nawedzaman <37767250+nawedzaman@users.noreply.github.com>
2023-12-30 10:29:38 +0530

    Create proxy-server.js

---
+++ proxy-server.js
@@ -0,0 +1,20 @@
+// proxy-server.js
+const express = require('express');
+const httpProxy = require('http-proxy');
+const cors = require('cors');
+
+const app = express();
+const apiProxy = httpProxy.createProxyServer();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Proxy route for Swiggy API
+app.get('/swiggy-api', (req, res) => {
+  apiProxy.web(req, res, { target: 'https://www.swiggy.com', changeOrigin: true });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Proxy server listening on port ${PORT}`);
+});
